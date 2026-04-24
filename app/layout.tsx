1436f2c476e22c9aa495c3f6119a2ae2dcb46127
craigmindset <irediacraig@gmail.com>
2026-04-24 14:30:22 +0100

    uet light to default theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className} suppressHydrationWarning>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <AuthProvider>
             <AdminProvider>
               {children}
